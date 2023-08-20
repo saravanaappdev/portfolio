fbc3aabd16771bf313714ab900fe75025bc50f5a
Saravana kumar <saravanakumar@Saravanas-MacBook-Air.local>
2023-08-20 18:35:45 +0530

    Updated the about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,6 +5,7 @@ const About = () => {
     <div
       name="about"
       className="w-full h-full bg-gradient-to-b from-gray-800 to-black text-white"
+      style={{fontFamily: "fantasy"}}
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full">
         <div className="pb-8">
@@ -13,42 +14,67 @@ const About = () => {
           </p>
         </div>
         <p className="text-xl mt-5">
-          Contrary to popular belief, Lorem Ipsum is not simply random text. It
-          has roots in a piece of classical Latin literature from 45 BC, making
-          it over 2000 years old. Richard McClintock, a Latin professor at
-          Hampden-Sydney College in Virginia, looked up one of the more obscure
-          Latin words, consectetur, from a Lorem Ipsum passage, and going
-          through the cites of the word in classical literature, discovered the
-          undoubtable source. Lorem Ipsum comes from sections 1.10.32 and
-          1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and
-          Evil) by Cicero, written in 45 BC. This book is a treatise on the
-          theory of ethics, very popular during the Renaissance. The first line
-          of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in
-          section 1.10.32. The standard chunk of Lorem Ipsum used since the
-          1500s is reproduced below for those interested. Sections 1.10.32 and
-          1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also
-          reproduced in their exact original form, accompanied by English
-          versions from the 1914 translation by H. Rackham.
+        👋 Greetings! 🌟 I'm Saravanakumar, a dedicated software engineer 🚀
+          driven by a passion for technology 🖥️ and a commitment to creating
+          impactful digital solutions 💡. With a career spanning various roles
+          and domains, I've had the privilege of contributing to transformative
+          projects 🛠️ and honing a diverse skill set 🛡️⚙️.
         </p>
         <br />
         <p className="text-xl">
-          Contrary to popular belief, Lorem Ipsum is not simply random text. It
-          has roots in a piece of classical Latin literature from 45 BC, making
-          it over 2000 years old. Richard McClintock, a Latin professor at
-          Hampden-Sydney College in Virginia, looked up one of the more obscure
-          Latin words, consectetur, from a Lorem Ipsum passage, and going
-          through the cites of the word in classical literature, discovered the
-          undoubtable source. Lorem Ipsum comes from sections 1.10.32 and
-          1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and
-          Evil) by Cicero, written in 45 BC. This book is a treatise on the
-          theory of ethics, very popular during the Renaissance. The first line
-          of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in
-          section 1.10.32. The standard chunk of Lorem Ipsum used since the
-          1500s is reproduced below for those interested. Sections 1.10.32 and
-          1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also
-          reproduced in their exact original form, accompanied by English
-          versions from the 1914 translation by H. Rackham.
+        👨‍💻 Currently, I'm proud to be a Software Engineer at Trane Technologies,
+          a pioneering force in the field of HVAC systems 🏭. Since February
+          2023, I've been an integral part of the team, crafting React.js magic
+          ✨, HVAC engineering marvels ❄️, home automation wonders 🏠, IoT
+          enchantments 🌐, and more. Based in the vibrant heart of Chennai, Tamil
+          Nadu, India 🇮🇳, I thrive on collaborating with a dynamic team of
+          like-minded professionals 👥.
         </p>
+        <br />
+        <p className="text-xl">
+          🛠️ My journey into the world of software engineering began at
+          [Previous Company], where I served as a Software Engineer for a
+          rewarding period of one year and seven months, from November 2021 to
+          May 2023 📅. Nestled in Chennai's tech landscape, I had the
+          opportunity to contribute to a range of projects that expanded my
+          technical expertise 🚀.
+        </p>
+        <br />
+        <p className="text-xl">
+          🌐 Before that, I embarked on a fulfilling adventure as a Full Stack
+          Developer at Squash Apps 🎾. Spanning two years and five months from
+          August 2019 to December 2021, I delved into crafting holistic
+          solutions 🌈 that seamlessly blended front-end artistry 🎨 with
+          back-end finesse 🛡️.
+        </p>
+        <br />
+        <p className="text-xl">
+          🌟 The seeds of my journey were sown during a Technical Support
+          Engineer Internship at Amazon 🌴. From September 2018 to June 2019,
+          I navigated through tech intricacies, nurturing a problem-solving
+          mindset 🧩 and a customer-centric approach 🤝.
+        </p>
+        <br />
+
+        <p className="text-xl">
+          🚀 Fueled by a fervor for learning, adapting, and innovating, my
+          portfolio is a testament to my journey. It showcases endeavors that
+          encompass my versatile skill set ⚙️🔧 and unwavering commitment to
+          making a positive difference 🌟. From elegant React.js symphonies 🎵 to
+          the intricate dance of IoT and home automation 💃, I relish challenges
+          that expand my horizons 🚀.
+        </p>
+        <br />
+
+        <p className="text-xl">
+          🤝 Whether you're a fellow enthusiast, a potential collaborator, or
+          simply intrigued by my journey, I invite you to connect 🤗. Let's
+          explore the boundless possibilities that technology presents, crafting
+          solutions that shape a brighter future 🌅. Feel free to reach out—I'm
+          always excited to embark on new adventures! 🌌
+        </p>
+        <br />
+        <br />
       </div>
     </div>
   );
